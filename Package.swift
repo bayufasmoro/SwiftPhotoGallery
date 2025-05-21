9f66432de1672da164d6e8ba1e867bd32f3ce2ef
Bayu F Asmoro <bayufa21@gmail.com>
2025-05-21 16:02:25 +0700

    Add SPM support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPhotoGallery",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "SwiftPhotoGallery",
+                 targets: ["SwiftPhotoGallery"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftPhotoGallery",
+            path: "Pod/Classes"
+        )
+    ]
+)
